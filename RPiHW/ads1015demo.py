c7360a9bf7eff289b213f7575a14cb1ce1fdf389
David C. <devish.c@gmail.com>
2026-04-23 13:06:52 -0700

    updating ads demo --DC

--- RPiHW/ads1015demo.py
+++ RPiHW/ads1015demo.py
@@ -29,9 +29,30 @@ print(f"{'Voltage (V)':>10}")
 #chan.voltage
 #chan.value
 
+# 12 bit: 0-2047
+
 while True:
-    # Read voltage directly
-    print("CH0: %.02f \t CH1: %.02f \t CH2: %.02f \t CH3: %.02f\n"%(chan0.voltage,chan1.voltage,chan2.voltage,chan3.voltage))
-    print("CH0: %.02f \t CH1: %.02f \t CH2: %.02f \t CH3: %.02f\n"%(chan0.value,chan1.value,chan2.value,chan3.value))
-    print("Solar cell voltage differential: %.02f"%(chan2.voltage-chan3.voltage))
+
+    c0VAL = chan0.value
+    c0VOL = chan0.voltage
+    c0RAW = (chan0.value >> 4)
+    
+    c1VAL = chan1.value
+    c1VOL = chan1.voltage
+    c1RAW = (chan1.value >> 4)
+    
+    c2VAL = chan2.value
+    c2VOL = chan2.voltage
+    c2RAW = (chan2.value >> 4)
+    
+    c3VAL = chan3.value
+    c3VOL = chan3.voltage
+    c3RAW = (chan3.value >> 4)
+    
+    
+    print("VOLTAGE: \t CH0: %.02f \t CH1: %.02f \t CH2: %.02f \t CH3: %.02f"%(c0VOL,c1VOL,c2VOL,c3VOL))
+    print("VALUE: \t CH0: %.02f \t CH1: %.02f \t CH2: %.02f \t CH3: %.02f"%(c0VAL,c1VAL,c2VAL,c3VAL))
+    print("RAW: \t CH0: %.02f \t CH1: %.02f \t CH2: %.02f \t CH3: %.02f"%(c0RAW,c1RAW,c2RAW,c3RAW))
+    print("Solar cell voltage differential: %.02f"%(c2VOL-c3VOL))
+    print("Solar cell ADC     differential: %.02f\n"%(c2VAL-c3VAL))
     time.sleep(1)
